edbaf2b502ed692fec831d7505b246994e62469c
tazmeen ilsa <tazmeenilsa@gmail.com>
2021-06-29 00:03:29 +0500

    upload

---
+++ android/app/src/main/kotlin/com/example/assign_4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.assign_4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:assign_4/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Calculator(),
+      ),
+    );
+  }
+}
